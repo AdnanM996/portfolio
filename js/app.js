7aa434005d6670f4220e46797b38b998f78233bc
Adnan Musinovic <adnanmusinovic98@gmail.com>
2020-02-18 20:56:25 +0100

    added pointer

--- js/app.js
+++ js/app.js
@@ -13,3 +13,15 @@ const nav = document.querySelector('.nav');
 menu.addEventListener('click', () => {
     nav.classList.toggle('open-nav');
 });
+
+//pointer
+
+const pointer = document.querySelector('.pointer');
+
+window.addEventListener('mousemove', (e) => {
+    xPosition = e.pageX + 'px';
+    yPosition = e.pageY + 'px';
+
+    pointer.style.left = xPosition;
+    pointer.style.top = yPosition;
+});
